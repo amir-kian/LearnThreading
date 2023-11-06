dcc8055857e8b177b724a1abc44124a21cf172c9
AmirHosein Javaheri Kian <A.Javaheri@Gan.local>
2023-11-06 12:37:20 +0330

    adding for and back

---
+++ Threading/ForegroundThread/Program.cs
@@ -0,0 +1,21 @@
+﻿static void DoWork()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Thread.Sleep(100);
+        Console.WriteLine("Foreground thread running...");
+
+    }
+}
+
+var t = new Thread(DoWork);
+t.Start();
+
+
+// Do some work in the main thread
+for (int i = 0; i < 2; i++)
+{
+    Thread.Sleep(100);
+    Console.WriteLine("Main thread running...");
+
+}
