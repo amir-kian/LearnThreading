5b60e7ec96424961f7141f5737e3b6654f311035
AmirHosein Javaheri Kian <A.Javaheri@Gan.local>
2023-11-06 13:00:59 +0330

    add treadpool

---
+++ Threading/Threadpool/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+static void CheckHttpStatus(string url)
+{
+    HttpClient client = new();
+    var response = client.GetAsync(url).Result;
+    WriteLine($"The HTTP status code of {url} is {response.StatusCode}");
+
+}
+
+List<string> urls = new()
+{
+    "https://www.google.com/",
+    "https://www.duckduckgo.com/",
+    "https://www.yahoo.com/",
+};
+
+foreach (var url in urls)
+{
+    ThreadPool.QueueUserWorkItem((state) => CheckHttpStatus(url));
+}
+
+
+// wait for all thread to complete
+// and press a key
+Console.Read();
